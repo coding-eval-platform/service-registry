96cd88a9406d36a5c4e236babd8d4b121020f3a2
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-27 03:17:03 -0300

    Add Eureka
    - Setup the eureka server, including Spring Cloud stuff

--- src/main/java/ar/edu/itba/cep/service_registry/Application.java
+++ src/main/java/ar/edu/itba/cep/service_registry/Application.java
@@ -3,11 +3,13 @@ package ar.edu.itba.cep.service_registry;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Bootstrap class.
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
 
     /**
